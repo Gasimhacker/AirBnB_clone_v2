683d95c06d84d986882ebe20d71d2967a3ea9c8a
Mohammed Ahmed <ma2286572@gmail.com>
2024-04-05 02:26:14 +0000

    Add the print statement

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -36,6 +36,7 @@ def do_deploy(archive_path):
         run(f'rm -rf {extraction_path}/web_static/')
         run('rm -rf /data/web_static/current')
         run(f'ln -s {extraction_path} /data/web_static/current')
+        print("New version deployed!")
         return True
     except Exception:
         return False
