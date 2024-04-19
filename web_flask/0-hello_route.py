00f30333d9680353ecaeae1c0776924343b87b77
Mohammed Ahmed <ma2286572@gmail.com>
2024-04-19 23:55:51 +0000

    Create the home route

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""A script to start the flask web application"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.route("/")
+def hello_hbnb():
+    return "Hello HBNB!"
+
+if (__name__ == "__main__"):
+    app.run(host="0.0.0.0")
