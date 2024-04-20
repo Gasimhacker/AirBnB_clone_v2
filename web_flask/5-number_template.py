eea8ffb735507e489e537b43c84ab8448f03a65f
Mohammed Ahmed <ma2286572@gmail.com>
2024-04-20 02:07:00 +0000

    Display the number template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+
+"""A script to start the flask web application"""
+
+
+from flask import Flask
+from flask import render_template
+from markupsafe import escape
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def home():
+    """The home route"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """The hbnb route"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_is_fun(text):
+    """The c route"""
+    text = text.replace("_", " ")
+    return f"C {escape(text)}"
+
+
+@app.route("/python/")
+def python_without_subpath():
+    """The python route"""
+    return "Python is cool"
+
+
+@app.route("/python/<text>")
+def python_is_cool(text):
+    """The python route with text"""
+    text = text.replace("_", " ")
+    return f"Python {escape(text)}"
+
+
+@app.route("/number/<int:n>")
+def is_number(n):
+    """The number route"""
+    return f"{escape(n)} is a number"
+
+
+@app.route("/number_template/<int:n>")
+def number_template(n):
+    """Display the number template"""
+    return render_template('5-number.html', n=n)
+
+
+if (__name__ == "__main__"):
+    app.run(host="0.0.0.0")
