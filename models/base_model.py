a30041dc7d4e65601c8e13f1caef0d241c376db2
Mohammed Ahmed <ma2286572@gmail.com>
2024-04-20 08:31:45 +0000

    Add the close method

--- models/base_model.py
+++ models/base_model.py
@@ -35,7 +35,9 @@ class BaseModel:
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self).__name__))
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        d = self.__dict__.copy()
+        d.pop("_sa_instance_state", None)
+        return '[{}] ({}) {}'.format(cls, self.id, d)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
