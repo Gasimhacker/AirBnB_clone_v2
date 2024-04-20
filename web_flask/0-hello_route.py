63c2a6a844f1540ac491bead550d1a7ba5b3851e
Mohammed Ahmed <ma2286572@gmail.com>
2024-04-20 00:00:01 +0000

    Fix th pycode style

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,10 +8,12 @@ from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route("/")
 def hello_hbnb():
     """The home route"""
     return "Hello HBNB!"
+
 
 if (__name__ == "__main__"):
     app.run(host="0.0.0.0")
